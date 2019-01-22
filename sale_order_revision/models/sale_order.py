48c5da3ede0dd7f4305c9e2f466fd7729f440f31
John <linyinhuan@139.com>
2019-01-22 12:54:03 +0800

    提供中文翻译

--- sale_order_revision/models/sale_order.py
+++ sale_order_revision/models/sale_order.py
@@ -3,8 +3,7 @@
 # Copyright 2018 Dreambits Technologies Pvt. Ltd. (<http://dreambits.in>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
-from odoo.tools.translate import _
+from odoo import api, fields, models, _
 
 
 class SaleOrder(models.Model):
